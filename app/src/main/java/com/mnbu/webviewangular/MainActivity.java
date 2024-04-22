4fae4d42165a42945b4cf46919e0bb4dd71d6182
Olan <75664520+lemaw@users.noreply.github.com>
2024-04-22 21:50:36 +0700

    Initial commit

---
+++ app/src/main/java/com/mnbu/webviewangular/MainActivity.java
@@ -0,0 +1,55 @@
+package com.mnbu.webviewangular;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView _webView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        _webView1 = (WebView) findViewById(R.id.webView1);
+
+        WebViewClient webViewClient = new WebViewClient();
+        _webView1.setWebViewClient(webViewClient);
+
+        WebChromeClient webChromeClient = new WebChromeClient();
+        _webView1.setWebChromeClient(webChromeClient);
+
+        WebSettings webSettings = _webView1.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        WebAppInterface webAppInterface = new WebAppInterface(this, MainActivity.this);
+        _webView1.addJavascriptInterface(webAppInterface, "Android");
+
+        String url = "https://stmikpontianak.net/011100862/angular011100862";
+        _webView1.loadUrl(url);
+    }
+}
---
+++ app/src/main/java/com/mnbu/webviewangular/WebAppInterface.java
@@ -0,0 +1,64 @@
+package com.mnbu.webviewangular;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+
+import android.os.Build;
+import android.provider.MediaStore;
+import android.webkit.JavascriptInterface;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+
+public class WebAppInterface {
+    private Activity _activity;
+    private Context _context;
+
+    public WebAppInterface(Context context, Activity activity){
+        _context = context;
+        _activity = activity;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @JavascriptInterface
+    public void showNotification(String title, String message){
+        NotificationChannel channel = new NotificationChannel("MNBUChannel", "MNBU", NotificationManager.IMPORTANCE_DEFAULT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(_context.getApplicationContext())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setChannelId(channel.getId());
+
+        NotificationManager manager = (NotificationManager) _context.getSystemService(NOTIFICATION_SERVICE);
+        manager.createNotificationChannel(channel);
+        manager.notify(1, builder.build());
+    }
+    @JavascriptInterface
+    public void showCall(){
+        Intent teleponIntent = new Intent(Intent.ACTION_DIAL);
+        _context.startActivity(teleponIntent);
+    }
+
+    @JavascriptInterface
+    public void showWhatsApp(){
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.setComponent(new ComponentName("com.whatsapp", "com.whatsapp.Main"));
+        _context.startActivity(intent);
+    }
+
+    @JavascriptInterface
+    public void showCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        _context.startActivity(intent);
+    }
+}
